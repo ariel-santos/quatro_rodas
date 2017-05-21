2daa8f454e37f62175ec24e050ab527fc1a32944
WORK\Ariel <arielsantos.ac@gmail.com>
2017-05-21 20:48:30 -0300

    topo
    Adicionado nova imagem para publicidade em mobile
    adicionada icones para menu mobile

--- header-topo.php
+++ header-topo.php
@@ -1,13 +1,19 @@
 <div id="topo">
 	<div class="row center" id="container-anuncio">
-		<div class="col s12 m8 offset-m2">
-			<a href="#!"><img src="<?php echo get_template_directory_uri(); ?>/media/anuncio.jpg" alt=""></a>
+		<div class="col s12 m8 offset-m2 hide-on-small-only">
+			<a href="#!"><img class="responsive-img" src="<?php echo get_template_directory_uri(); ?>/media/anuncio.jpg" alt=""></a>
+		</div>
+		<div class="col s12 m8 offset-m2 hide-on-med-and-up">
+			<a href="#!"><img class="responsive-img" src="<?php echo get_template_directory_uri(); ?>/media/anuncio2.jpg" alt=""></a>
 		</div>
 	</div>
 	<?php if ( has_nav_menu( 'primary' ) ) : ?>
 	<div class="row no-margin red" id="container-menu-topo">
 		<nav class="vermelho">
-			<div class="col s12 m10 nav-wrapper">
+			<div class="col s2 hide-on-med-and-up nav-wrapper center" >
+				<a href="#!" class="mobile-ico"><img src="<?php echo get_template_directory_uri();?>/media/lupa.png" alt="">	</a>
+			</div>
+			<div class="col s8 m10 nav-wrapper">
 				<a href="#" class="brand-logo"><img src="<?php echo get_template_directory_uri(); ?>/media/logo.png" alt=""></a>
 				<?php 
 					wp_nav_menu( array(
@@ -18,7 +24,11 @@
 					 ) );
 				?>
 			</div>
-			<div class="col m2 hide-small-only">
+			<div class="col s2 hide-on-med-and-up nav-wrapper center">
+				<a href="#!" id="mobile-ico-menu" class="mobile-ico"><img src="<?php echo get_template_directory_uri();?>/media/x.png" alt=""></a>
+			</div>
+			
+			<div class="col s2 hide-on-small-only nav-wrapper">
 				pesqusa	
 			</div>
 		</nav>
@@ -32,7 +42,7 @@
 				?>
 			</div>
 		</div>
-		<div class="col s12 mega-menu preto hide" id="mega-menu-Testes">
+		<div class="col s12 mega-menu preto hide hide-small-only" id="mega-menu-Testes">
 			<div class="col m2 sub-menu">
 				<ul>
 					<li><a href="#tudo">Ver tudo de testes</a></li>
@@ -63,23 +73,33 @@
 				<h1>BMW 420i Cabriolet</h1>
 			</div>
 		</div>
-		</div>
-		<div class="row no-margin" id="container-mais-acessados">
-			<ul>
-				<li><a href="#!">+ACESSADOS</a>	<span><img src="<?php echo get_template_directory_uri();?>/media/menu-detalhe.png" alt=""></span></li>
-				<li><a href="#!">SALÃO DO AUTOMÓVEL</a></li>
-				<li><a href="#!">RENEGADE</a></li>
-				<li><a href="#!">NOVO SANDERO</a></li>
-				<li><a href="#!">NOVO FOX</a></li>
-				<li><a href="#!">NOVO KA</a></li>
-				<li><a href="#!">HB 20</a></li>
-				<li><a href="#!">DUSTER</a></li>
-				<li><a href="#!">GOLF</a></li>
-				<li><a href="#!">COROLLA</a></li>
-				<li><a href="#!">CIVIC</a></li>
-				<li><a href="#!">IA-ZI</a></li>
-			</ul>
-		</div>
-		
-		<?php endif; ?>	
+	</div>
+	<div class="row hide" id="mobile-menu">
+		<?php 
+			wp_nav_menu( array(
+				'theme_location' => 'primary',
+				'menu_id' => 'nav-mobile',
+				'menu_class'     => ''
+			 ) );
+		?>
+	</div>
+	
+	<div class="row no-margin hide-on-small-only" id="container-mais-acessados">
+		<ul>
+			<li><a href="#!">+ACESSADOS</a>	<span><img src="<?php echo get_template_directory_uri();?>/media/menu-detalhe.png" alt=""></span></li>
+			<li><a href="#!">SALÃO DO AUTOMÓVEL</a></li>
+			<li><a href="#!">RENEGADE</a></li>
+			<li><a href="#!">NOVO SANDERO</a></li>
+			<li><a href="#!">NOVO FOX</a></li>
+			<li><a href="#!">NOVO KA</a></li>
+			<li><a href="#!">HB 20</a></li>
+			<li><a href="#!">DUSTER</a></li>
+			<li><a href="#!">GOLF</a></li>
+			<li><a href="#!">COROLLA</a></li>
+			<li><a href="#!">CIVIC</a></li>
+			<li><a href="#!">IA-ZI</a></li>
+		</ul>
+	</div>
+
+	<?php endif; ?>	
 </div>
